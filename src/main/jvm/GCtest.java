5c8c4f22071255847bee54faa8e1e8795149b771
shuyunxiang <572130390@qq.com>
2022-08-27 11:09:17 +0800

    新对象优先进入eden区

---
+++ src/main/jvm/GCtest.java
@@ -0,0 +1,35 @@
+package jvm;
+
+/**
+ * Java虚拟机垃圾回收测试
+ */
+public class GCtest {
+
+    public static void main(String[] args) {
+        // 测试新对象优先
+        testAllocation();
+
+    }
+
+    private static final int _1MB = 1024 * 1024;
+
+    /**
+     * VM参数：
+     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseSerialGC
+     * -Xms20M 最小堆内存
+     * -Xmx20M 最大堆内存
+     * -Xmn10M 新生代内存
+     * -XX:+PrintGCDetails 打印详细GC日志
+     * -XX:SurvivorRatio=8 eden/survivor比率
+     * -XX:+UseSerialGC 指定垃圾内存回收器
+     */
+    public static void testAllocation() {
+        byte[] allocation1, allocation2, allocation3, allocation4, allocation5;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        // 出现一次Minor GC
+        allocation4 = new byte[2 * _1MB];
+        allocation5 = new byte[2 * _1MB];
+    }
+}
