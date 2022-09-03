03684f7ed44737b80d3edd6ba074b5cb681d0ebc
shuyunxiang <572130390@qq.com>
2022-09-03 16:38:41 +0800

    公平锁和非公平锁

--- src/main/thread/createthread/ThreadTest.java
+++ src/main/thread/createthread/ThreadTest.java
@@ -1,9 +1,6 @@
-package thread;
+package thread.createthread;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class ThreadTest {
 
---
+++ src/main/thread/lock/FairAndUnfairLockTest.java
@@ -0,0 +1,75 @@
+package thread.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairAndUnfairLockTest {
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+
+        Thread[] thread = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            thread[i] = new Thread(new Job(printQueue), "Thread " + i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            thread[i].start();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Job implements Runnable {
+
+    private PrintQueue printQueue;
+
+    public Job(PrintQueue printQueue) {
+        this.printQueue = printQueue;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s: 进入线程\n", Thread.currentThread().getName());
+        printQueue.printJob(new Object());
+        System.out.printf("%s: 离开线程\n", Thread.currentThread().getName());
+    }
+}
+
+class PrintQueue {
+
+    private final Lock queueLock = new ReentrantLock(true);
+
+    public void printJob(Object document) {
+
+        // 抢锁
+        queueLock.lock();
+        try {
+            long duration = (long) (Math.random() * 10000);
+            System.out.printf("%s: 任务1 %d seconds\n",
+                    Thread.currentThread().getName(), (duration / 1000));
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+
+        // 再次抢锁
+        queueLock.lock();
+        try {
+            long duration = (long) (Math.random() * 10000);
+            System.out.printf("%s: 任务2 %d seconds\n",
+                    Thread.currentThread().getName(), (duration / 1000));
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+
+}
