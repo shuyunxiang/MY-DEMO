9f9f6812fe285c51168083f88c98218424266319
SHUYUNXIANG <572130390@qq.com>
2023-02-25 17:15:20 +0800

    补充TTL的使用方法

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -1,5 +1,17 @@
+package main;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("测试");
+        TransmittableThreadLocal<String> context = new TransmittableThreadLocal<>();
+
+        // 在父线程中设置
+        context.set("value-set-in-parent");
+
+        // 在子线程中可以读取，值是"value-set-in-parent"
+        String value = context.get();
+
+        System.out.println(value);
     }
 }
